db016205356756a2d684068d6d5f9c36a87d5eff
jav.barrios89@gmail.com <jav.barrios89@gmail.com>
2024-03-14 20:35:58 +0000

    Created LICENSE.txt; Edited settings, base, theme/parameters.yaml and home

--- client_code/base/__init__.py
+++ client_code/base/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import baseTemplate
 from anvil import *
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -9,5 +10,23 @@ class base(baseTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+     
+    # Check if a user is logged in when the app starts
+    if anvil.users.get_user() is None:
+      # If no user is logged in, show the login form
+      self.login()
+      
     self.content_panel.add_component((home()))
-    # Any code you write here will run before the form opens.
+
+  def login(self):
+    # This method shows the built-in Anvil login form
+    user = anvil.users.login_with_form()
+    if user is not None:
+      # User logged in successfully
+      # Here you can handle what happens after a successful login
+      pass
+    else:
+      # The user closed the login form without logging in
+      # You may want to handle this scenario, e.g., exit the app or re-show login
+      anvil.server.call('anvil.server.reset_environment')
+      self.login()
--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -1,8 +1,9 @@
-from ._anvil_designer import baseTemplate
+from ._anvil_designer import homeTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.users
 
 class home(homeTemplate):
   def __init__(self, **properties):
